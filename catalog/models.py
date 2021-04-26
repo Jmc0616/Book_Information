52033c7f3e0c7206641071e7759d13cc98d00b2c
myeongcheol Jeong <jmch0616@msecure.co>
2021-04-26 18:20:31 +0900

    make model migrations

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,86 @@
 from django.db import models
+from django.urls import reverse 
+import uuid 
+
+# 필드
+class MyModelName(models.Model): 
+    my_field_name = models.CharField(max_length=20, help_text='Enter field documentation')
+#메타테이터
+class Meta:
+    ordering = ['-my_field_name']
+#메소드 
+def get_absolute_url(self):
+    return reverse('model-detail-view', args=[str(self.id)])
+
+def __str__(self):
+    return self.field_name
 
 # Create your models here.
+
+# 장르모델 > 책 카테고리에 관한 정보 
+class Genre(models.Model):
+    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
+
+    def __str__(self):
+        return self.name
+
+# 책모델 > 책에 대한 모든 정보를 보여줌 
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+
+    summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
+    isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+
+    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)])
+
+
+# 책인스턴스 > 복사본이 사용 가능한 지, 언제 되돌려 받을 수 있는지, 출판사 또는 버전 세부사항, 책의 고유 id정보
+class BookInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book availability',
+    )  
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        return f'{self.id} ({self.book.title})'
+
+# 저자 모텔 > 이름 , 성, 생일, 사망일등 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
