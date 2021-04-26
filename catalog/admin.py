817795ac3ae0d2f8d3b68e6872548f9a4dfdb879
myeongcheol Jeong <jmch0616@msecure.co>
2021-04-26 19:02:54 +0900

    make main page -ing

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+# 모델등록 register
+from catalog.models import Author, Genre, Book, BookInstance
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from catalog import views
+
 urlpatterns = [
     
+]
+
+urlpatterns + [
+    path('', views.index, name='index')
 ]
